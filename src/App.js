4a9e2f29bf77c09e8cb1613fb5eab337b259bf43
Julia Wos <juliawos22@gmail.com>
2022-09-26 12:16:13 +0200

    cleaned up the code, fix an issue with costs not summing up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,116 @@
 import './App.css';
 import Input from './components/input/Input.js';
+import { useState } from 'react';
 
 function App() {
+  const possibleRoutes = [
+    'AB',
+    'AC',
+    'AD',
+    'BE',
+    'CD',
+    'CF',
+    'DE',
+    'EB',
+    'EA',
+    'FD',
+  ];
+
+  const possibleRoutesWithCosts = [
+    { name: 'AB', cost: 1 },
+    { name: 'AC', cost: 4 },
+    { name: 'AD', cost: 10 },
+    { name: 'BE', cost: 3 },
+    { name: 'CD', cost: 4 },
+    { name: 'CF', cost: 2 },
+    { name: 'DE', cost: 1 },
+    { name: 'EB', cost: 3 },
+    { name: 'EA', cost: 2 },
+    { name: 'FD', cost: 1 },
+  ];
+
+  //state handling
+  const [input, setInput] = useState([]);
+
+  const [sum, setSum] = useState(0);
+
+  //adding chosen cities
+  const handleInputChange = (e) => {
+    setInput([...input, e.target.value]);
+  };
+
+  //resetting the input array
+  const resetInput = () => {
+    setInput([]);
+    setSum(0);
+  };
+
+  //creating pairs to compare the cities with
+  let pairs = [];
+  const createPairs = () => {
+    for (var i = 0; i < input.length - 1; i++) {
+      var currentElement = input[i];
+      var nextElement = input[i + 1];
+      pairs.push(currentElement + nextElement);
+    }
+
+    return pairs;
+  };
+
+  //checking which pairs are possible
+  let match = [];
+  const checkForMatches = () => {
+    createPairs();
+    for (let i = 0; i < possibleRoutes.length; i++) {
+      for (let j = 0; j < pairs.length; j++) {
+        if (possibleRoutes[i] === pairs[j]) {
+          //       // Return if common element found
+          match.push(pairs[j]);
+        }
+      }
+    }
+    return match;
+  };
+
+  //compare possible matches with original pairs from the input, only accept if all exist, if not "route not possible"
+  let totalCost = 0;
+
+  function areArraysMatchig() {
+    checkForMatches();
+    //check if all routes are possible
+    if (match.sort().toString() === pairs.sort().toString()) {
+      //loop through array containing the costs
+      for (var i = 0; i < possibleRoutesWithCosts.length; i++) {
+        //loop through chosen routes to select correct prices
+        for (let j = 0; j < pairs.length; j++) {
+          if (possibleRoutesWithCosts[i].name === pairs[j]) {
+            totalCost += possibleRoutesWithCosts[i].cost;
+          }
+        }
+      }
+      setSum(totalCost);
+      return sum;
+    } else {
+      setSum('No such route possible, reset to try again');
+    }
+  }
+
+  const calculateDeliveryCost = (e) => {
+    e.preventDefault();
+
+    areArraysMatchig();
+  };
   return (
     <div className="App">
-      <h1>Calculate delivery costs</h1>
-      <Input label={'Your Route: '} />
+      <h1>Calculate your delivery costs</h1>
+      <Input
+        label={'Your Route: '}
+        calculateDeliveryCost={calculateDeliveryCost}
+        handleInputChange={handleInputChange}
+        resetInput={resetInput}
+        input={input}
+        sum={sum}
+      />
     </div>
   );
 }
--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,112 +1,6 @@
 import './Input.css';
-import { useState } from 'react';
 
 const Input = (props) => {
-  const possibleRoutes = [
-    'AB',
-    'AC',
-    'AD',
-    'BE',
-    'CD',
-    'CF',
-    'DE',
-    'EB',
-    'EA',
-    'FD',
-  ];
-
-  const possibleRoutesWithCosts = [
-    { name: 'AB', cost: 1 },
-    { name: 'AC', cost: 4 },
-    { name: 'AD', cost: 10 },
-    { name: 'BE', cost: 3 },
-    { name: 'CD', cost: 4 },
-    { name: 'CF', cost: 2 },
-    { name: 'DE', cost: 1 },
-    { name: 'EB', cost: 3 },
-    { name: 'EA', cost: 2 },
-    { name: 'FD', cost: 1 },
-  ];
-
-  //state handling
-  const [input, setInput] = useState([]);
-  console.log(input);
-
-  //adding chosen cities
-  const handleInputChange = (e) => {
-    setInput([...input, e.target.value]);
-  };
-
-  //resetting the input array
-  const resetInput = () => {
-    setInput([]);
-    sum = 0;
-  };
-
-  //creating pairs to compare the cities with
-  let pairs = [];
-  const createPairs = () => {
-    for (var i = 0; i < input.length - 1; i++) {
-      var currentElement = input[i];
-      var nextElement = input[i + 1];
-      pairs.push(currentElement + nextElement);
-    }
-    console.log('pairs', pairs);
-
-    return pairs;
-  };
-
-  //checking which pairs are possible
-  let match = [];
-  const checkForMatches = () => {
-    createPairs();
-    for (let i = 0; i < possibleRoutes.length; i++) {
-      for (let j = 0; j < pairs.length; j++) {
-        console.log(possibleRoutes[i], pairs[j]);
-        if (possibleRoutes[i] === pairs[j]) {
-          //       // Return if common element found
-          match.push(pairs[j]);
-        }
-      }
-    }
-    console.log('matches: ', match);
-    return match;
-  };
-
-  //compare possible matches with original pairs from the input, only accept if all exist, if not "route not possible"
-  let sum = 0;
-
-  function areArraysMatchig() {
-    checkForMatches();
-    //check if all routes are possible
-    if (match.sort().toString() === pairs.sort().toString()) {
-      //loop through array containing the costs
-      for (var i = 0; i < possibleRoutesWithCosts.length; i++) {
-        console.log('possible', possibleRoutesWithCosts[i].name);
-        //loop through chosen routes to select correct prices
-        for (let j = 0; j < pairs.length; j++) {
-          console.log('Pairs possible', pairs[j]);
-          if (possibleRoutesWithCosts[i].name === pairs[j]) {
-            sum += possibleRoutesWithCosts[i].cost;
-            console.log('pairs', pairs[i]);
-            console.log('its working inside the loop');
-          }
-        }
-      }
-      console.log('sum', sum);
-      return sum;
-    } else {
-      console.log('not possible');
-      sum = 'No such route possible';
-    }
-  }
-
-  const calculateDeliveryCost = (e) => {
-    e.preventDefault();
-    console.log('calc', input);
-
-    areArraysMatchig();
-  };
 
   return (
     <div>
@@ -118,53 +12,71 @@ const Input = (props) => {
             value="A"
             id="optionA"
             className="route"
-            onClick={handleInputChange}
+            onClick={props.handleInputChange}
           />
           <input
             type="button"
             value="B"
             id="optionB"
             className="route"
-            onClick={handleInputChange}
+            onClick={props.handleInputChange}
           />
           <input
             type="button"
             value="C"
             id="optionC"
             className="route"
-            onClick={handleInputChange}
+            onClick={props.handleInputChange}
           />
           <input
             type="button"
             value="D"
             id="optionD"
             className="route"
-            onClick={handleInputChange}
+            onClick={props.handleInputChange}
           />
           <input
             type="button"
             value="E"
             id="optionE"
             className="route"
-            onClick={handleInputChange}
+            onClick={props.handleInputChange}
           />
           <input
             type="button"
             value="F"
             id="optionF"
             className="route"
-            onClick={handleInputChange}
+            onClick={props.handleInputChange}
           />
         </label>
-        <input type="button" value="RESET" onClick={resetInput} />
-        <input
-          type="submit"
-          value="Calculate delivery cost"
-          onClick={calculateDeliveryCost}
-        />
+
+          <input
+            type="submit"
+            value="CALCULATE DELIVERY COST"
+            onClick={props.calculateDeliveryCost}
+            className="submit calc"
+          />
+          <input
+            type="button"
+            value="RESET"
+            onClick={props.resetInput}
+            className="submit reset"
+          />
+
       </form>
       <div>
-        <p>CHOSEN ROUTE: {input}</p>
+        <p>{props.input.length === 1 ? 'Select at least two citites' : ''}</p>
+        <h3>CHOSEN ROUTE: {props.input}</h3>
+        <h2
+          className={
+            props.sum === 'No such route possible, reset to try again' ? 'error' : ''
+          }
+        >
+          {props.sum === 'No such route possible, reset to try again'
+            ? props.sum
+            : `Total delivery cost: ${props.sum}`}
+        </h2>
       </div>
     </div>
   );
